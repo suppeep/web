18b26be476388a4e3247d91d06996f549dec8dee
Lukas <master.doom.xii@gmail.com>
2022-05-18 11:00:06 +0200

    change path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
   plugins: [
     vue(),
     VitePWA({
@@ -20,27 +30,27 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "./src/assets/images/icon-192x192.png",
+            src: "icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "./src/assets/images/icon-256x256.png",
+            src: "icon-256x256.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "./src/assets/images/icon-384x384.png",
+            src: "icon-384x384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "./src/assets/images/icon-512x512.png",
+            src: "icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "./src/assets/images/icon-512x512.png",
+            src: "icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
